4822f7ccee9fdde9a8158c12e0d758c736f7bfc5
duytien241 <lucthuyvip@gmail.com>
2020-11-06 09:46:58 +0700

    update worker.js

--- worker.js
+++ worker.js
@@ -1,14 +1,14 @@
-var SCWorker = require('socketcluster/scworker');
-var express = require('express');
-var serveStatic = require('serve-static');
-var path = require('path');
-var morgan = require('morgan');
-var healthChecker = require('sc-framework-health-check');
-var scCodecMinBin = require('sc-codec-min-bin');
+var SCWorker = require("socketcluster/scworker");
+var express = require("express");
+var serveStatic = require("serve-static");
+var path = require("path");
+var morgan = require("morgan");
+var healthChecker = require("sc-framework-health-check");
+var scCodecMinBin = require("sc-codec-min-bin");
 
 class Worker extends SCWorker {
   run() {
-    console.log('   >> Worker PID:', process.pid);
+    console.log("   >> Worker PID:", process.pid);
     var environment = this.options.environment;
 
     var app = express();
@@ -17,27 +17,26 @@ class Worker extends SCWorker {
     var scServer = this.scServer;
     scServer.setCodecEngine(scCodecMinBin);
 
-    if (environment === 'dev') {
+    if (environment === "dev") {
       // Log every HTTP request.
       // See https://github.com/expressjs/morgan for other available formats.
-      app.use(morgan('dev'));
+      app.use(morgan("dev"));
     }
-    app.use(serveStatic(path.resolve(__dirname, 'public')));
+    app.use(serveStatic(path.resolve(__dirname, "public")));
 
     healthChecker.attach(this, app);
 
-    httpServer.on('request', app);
-    var count = 0
+    httpServer.on("request", app);
     scServer.on("connection", function (socket) {
       socket.on("user_message_evt", function (data) {
         socket.on(`user_uttered${data.id}`, function (data) {
-          scServer.exchange.publish("user_uttered", {id:data.id, message: data});
+          scServer.exchange.publish("user_uttered", {
+            id: data.id,
+            message: data,
+          });
         });
-  
       });
-      socket.on("disconnect", function () {
-        clearInterval(interval);
-      });
+      socket.on("disconnect", function () {});
     });
   }
 }
